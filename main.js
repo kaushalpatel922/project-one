f25fbd393e554193d8d3778237a6505f5379b333
kaushalpatel922 <kaushalpatel922@gmail.com>
2016-03-06 18:19:05 -0500

    updated project

--- main.js
+++ main.js
@@ -16,116 +16,117 @@ var numOfMoves = 0;
 var xCount = 0;
 var oCount = 0;
 var tieCount = 0;
-currentMove = "X";
+var currentMove = "X";
 
-function changeBox(cell) {
-  if (cell.value == "") {
-    cell.value = symbol;
-    numOfMoves++;
-    document.currentMove.value = currentMove;
-  }
-    else {
-    alert("This box is already selected! Please select another box!");
-  }
 
-  if(checkWin()) {
-    alert("Player " + cell.value + "wins!");
-    if (symbol == "X") {
-      xCount++;
-    document.currentMove.value = xCount;
+function changeBox (cell) {
+    if (cell.value == "") {
+        cell.value = symbol;
+        numOfMoves++;
     }
     else {
-      oCount++;
-      document.currentMove.value = oCount;
+        alert("This box is already selected! Please select another box!");
     }
-    clearBtn (); // -- needs to define
-    return;
-  }
 
-  if(numOfMoves == 9) {
-    alert("It's a tie!");
-    tieCount++;
-    document.currentMove.value = tieCount;
-  }
-    else {
-      changeSymbol(); //--needs to define
+    if (checkWin()) {
+        alert("Player " + cell.value + " wins!");
+        if (symbol == "X") {
+            xCount++;
+        } else {
+            oCount++;
+        }
+        clearBtn();
+        updateScore();
+        return;
     }
+
+    if (numOfMoves == 9) {
+        alert("It's a tie!");
+        tieCount++;
+        clearBtn();
+        return;
+    }
+        else {
+        changeSymbol();
+        }
 }
 
 function changeSymbol() {
-  if(symbol == "X") {
-    symbol == "O";
-    currentMove = "O"
-  }
-  else {
-    symbol = "X";
-    currentMove = "X";
-  }
+    if (symbol == "X") {
+        symbol = "O";
+        currentMove = "O"
+    } else {
+        symbol = "X";
+        currentMove = "X";
+    }
 }
 
-funciton checkWin() {
-  if((s1.value == "X") && (s2.value == "X") && (s3.value == "X"))
-    return true;
-  else if ((s4.value == "X") && (s5.value == "X") && (s6.value == "X"))
-    return true;
-  else if ((s7.value == "X") && (s8.value == "X") && (s9.value == "X"))
-    return true;
-  else if ((s1.value == "X") && (s4.value == "X") && (s7.value == "X"))
-    return true;
-  else if ((s2.value == "X") && (s5.value == "X") && (s8.value == "X"))
-    return true;
-  else if ((s3.value == "X") && (s6.value == "X") && (s9.value == "X"))
-    return true;
-  else if ((s1.value == "X") && (s5.value == "X") && (s9.value == "X"))
-    return true;
-  else if ((s3.value == "X") && (s5.value == "X") && (s7.value == "X"))
-    return true;
+function checkWin() {
+    with(document.simpleForm) {
+        if ((s1.value == "X") && (s2.value == "X") && (s3.value == "X"))
+            return true;
+        if ((s4.value == "X") && (s5.value == "X") && (s6.value == "X"))
+            return true;
+        if ((s7.value == "X") && (s8.value == "X") && (s9.value == "X"))
+            return true;
+        if ((s1.value == "X") && (s4.value == "X") && (s7.value == "X"))
+            return true;
+        if ((s2.value == "X") && (s5.value == "X") && (s8.value == "X"))
+            return true;
+        if ((s3.value == "X") && (s6.value == "X") && (s9.value == "X"))
+            return true;
+        if ((s1.value == "X") && (s5.value == "X") && (s9.value == "X"))
+            return true;
+        if ((s3.value == "X") && (s5.value == "X") && (s7.value == "X"))
+            return true;
 
-
-  else if ((s4.value == "O") && (s5.value == "O") && (s6.value == "O"))
-    return true;
-  else if ((s7.value == "O") && (s8.value == "O") && (s9.value == "O"))
-    return true;
-  else if ((s1.value == "O") && (s4.value == "O") && (s7.value == "O"))
-    return true;
-  else if ((s2.value == "O") && (s5.value == "O") && (s8.value == "O"))
-    return true;
-  else if ((s3.value == "O") && (s6.value == "O") && (s9.value == "O"))
-    return true;
-  else if ((s1.value == "O") && (s5.value == "O") && (s9.value == "O"))
-    return true;
-  else if ((s3.value == "O") && (s5.value == "O") && (s7.value == "O"))
-    return true;
-
+        if ((s1.value == "O") && (s2.value == "O") && (s3.value == "O"))
+            return true;
+        if ((s4.value == "O") && (s5.value == "O") && (s6.value == "O"))
+            return true;
+        if ((s7.value == "O") && (s8.value == "O") && (s9.value == "O"))
+            return true;
+        if ((s1.value == "O") && (s4.value == "O") && (s7.value == "O"))
+            return true;
+        if ((s2.value == "O") && (s5.value == "O") && (s8.value == "O"))
+            return true;
+        if ((s3.value == "O") && (s6.value == "O") && (s9.value == "O"))
+            return true;
+        if ((s1.value == "O") && (s5.value == "O") && (s9.value == "O"))
+            return true;
+        if ((s3.value == "O") && (s5.value == "O") && (s7.value == "O"))
+            return true;
+    }
 }
 
 function clearBtn() {
-
+    document.simpleForm.s1.value = "";
+    document.simpleForm.s2.value = "";
+    document.simpleForm.s3.value = "";
+    document.simpleForm.s4.value = "";
+    document.simpleForm.s5.value = "";
+    document.simpleForm.s6.value = "";
+    document.simpleForm.s7.value = "";
+    document.simpleForm.s8.value = "";
+    document.simpleForm.s9.value = "";
+    symbol = "X";
+    numOfMoves = 0;
 }
 
-
-
-
-
-
-
-
-var playButton = document.querySelector("#playTTT");
-playButton.addEventListener("click", function (){
-  // tttGame.play();
-  alert("Welcome to Tic Tac Toe!");
-});
-
-var tttGame = {
-  winningCombinations: [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]],
-  boxdivs: document.querySelectorAll(".box"),
-  userSelection: undefined,
+function erase() {
+    xCount = 0;
+    oCount = 0;
+    tieCount = 0;
+    alert("Nice!");
 }
 
-var boxOne = document.querySelector("#boxOne");
-boxOne.addEventListener("mouseover", function () {
-  this.innerHTML = "Hello World";
-  for (var i = 0; i < 1; i++) {
-    this.innerHTML = "";
-  }
-});
+function updateScore() {
+    if (xCount != 0) {
+        var x = document.getElementById('scoreOne');
+        x.innerHTML = xCount;
+    }
+    if (oCount != 0) {
+        var y = document.getElementById('scoreTwo');
+        y.innerHTML = oCount;
+    }
+}
